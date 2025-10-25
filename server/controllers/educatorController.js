d39a19ace731d1f0d16e05549b97594dda2cd86c
Jaynil <jaynil.gundecha@gmail.com>
2025-10-26 04:09:53 +0530

    ..

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,7 +1,10 @@
 import express from 'express'
 
 const studentToEdu = async (req, res) => {
-    console.log('request-structure :  => ' , req);//controller
+    // console.log('request-structure :  => ' , JSON.stringify(req));//controller
+    console.log('Auth info:', req.auth);      // or req.user
+    console.log('Auth ID:', req.auth?.userId); // if it exists
+
     res.json({success : true})
 }
 export default studentToEdu;
--- server/routes/educatorRouter.js
+++ server/routes/educatorRouter.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import studentToEdu from '../controllers/educatorController.js';
+import { requireAuth } from '@clerk/express';
 
 const eduRouter = express.Router();
 
-eduRouter.patch('/update-role', studentToEdu);
+// eduRouter.patch('/update-role', studentToEdu);
+eduRouter.patch('/update-role', requireAuth(), studentToEdu);
 
 export default eduRouter;
